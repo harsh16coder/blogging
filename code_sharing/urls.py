a55a0472e672d6b2331df1b0bdc89890a8cb30df
harsh16coder <78309251+harsh16coder@users.noreply.github.com>
2021-01-31 22:39:12 +0530

    Initial commit

---
+++ code_sharing/urls.py
@@ -0,0 +1,13 @@
+# newspaper_project/urls.py
+
+from django.contrib import admin
+from django.urls import path, include # new
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('users/', include('users.urls')), # new
+    path('users/', include('django.contrib.auth.urls')), # new
+    path('articles/',include('articles.urls')),
+    path('', include('pages.urls')), # new
+]
